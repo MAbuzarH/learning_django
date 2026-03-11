76ae27b2bcacbd3330ea67f84694c163fd6f2b22
MAbuzarH <shahabuzar172@gmail.com>
2026-03-11 16:27:23 +0500

    started portfolio project

--- project_11/myproject/blog/urls.py
+++ project_11/myproject/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="home"),
-     path('about/',views.about, name="about"),
+    path('home/',views.home, name="home"),
+     path('blog/',views.about, name="blog"),
 ]
--- project_11/myproject/blog/views.py
+++ project_11/myproject/blog/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-  return render(request,'home.html')
+  return render(request,'base.html')
 
 def about(request):
-  return render(request,'blog/about.html')
+  student_list = [
+    {"name":"Ali","class":"10th"},
+    {"name":"abuzar","class":"12th"},
+    {"name":"Asif","class":"10th"},
+  ]
+  return render(request,'blog.html',{'students':student_list})
