36d89759e98c24c850861508c3a6c24d8a3e59f3
MAbuzarH <103282484+MAbuzarH@users.noreply.github.com>
2026-04-01 18:01:09 +0500

    learned file uploading

---
+++ project_23/myproject/accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from .forms import ProfileForm
+from .models import Profile
+from django.contrib import messages
+
+
+# Create your views here.
+def upload_profile(request):
+  if request.method == 'POST':
+    form = ProfileForm(request.POST,request.FILES)
+    if form.is_valid():
+      form.save()
+      messages.success(request,'Profile uploaded successfully!')
+      return redirect('view_profile')
+    else:
+      messages.error(request,'Error uploading profile. Please try again.')
+  else:
+    form = ProfileForm()
+    return render(request,'accounts/upload_profile.html',{'form':form}) 
+  
+
+def view_profile(request):
+  profile = Profile.objects.all()
+  return render(request,'accounts/view_profile.html',{'profiles':profile})
